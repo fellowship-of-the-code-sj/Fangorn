3ef728b833c47626d5b95de6f0ff6e0d5109fcfc
Adnan Noori <noori.adnan@gmail.com>
2021-03-01 11:12:05 -0800

    - Updated index.js to included a controllers file to store helper functions

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-const apiRequest = require('./apiServer/apiRequest.js')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { retrieveAllRelatedItems } = require('./helperFunctions/helperFunction.js');
+const { relatedItems } = require('./controllers.js');
 
 const app = express();
 
@@ -13,31 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // Retrieves get request for endpoint /RelatedItems
-app.get('/RelatedItems', (req, res) => {
-  //makes get request to Atelier Products API
-  apiRequest.get(`/products/${req.query.itemId}/related`, (err, data) => {
-    if (err) {
-      res.status(err.response.status);
-      res.end();
-    } else {
-      retrieveAllRelatedItems(data.data, (err, productsData) => {
-        if (err) {
-          //check if error status exists
-          if (err.response) {
-            res.status(err.response.status);
-          } else {
-            res.status(404);
-          }
-          res.end();
-
-        } else {
-          res.status(data.status);
-          res.send(productsData);
-        }
-      })
-    }
-  });
-});
+app.get('/RelatedItems', relatedItems.get);
 
 const PORT = 404;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
