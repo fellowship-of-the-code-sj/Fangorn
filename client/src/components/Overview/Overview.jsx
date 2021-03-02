271928a3543fb9978a1ebdd9d3e8bccc63f1d021
Jaspreet Atwal <jsatwal25@gmail.com>
2021-03-01 22:51:05 -0800

    Overview now renders Product Information component dynamically with data from server

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -8,19 +8,25 @@ const Overview = (props) => {
   const [ product, setProduct ] = useState({});
   const [ styles, setStyles ] = useState({});
   const [ ratings, setRatings ] = useState({});
+  const [ style, setStyle ] = useState({});
 
   useEffect(() => {
     axiosHelper.get(`http://localhost:${port}/Overview`, {itemID: props.productID}, (results) => {
       setProduct(results.data.productObj);
       setStyles(results.data.stylesArr);
       setRatings(results.data.ratingsObj);
+      setStyle(results.data.stylesArr[0]);
     })
   }, [product])
-  
+
   return (
     <div className="overview">
       <h1>Overview:</h1>
-      <ProductInfo />
+      <ProductInfo 
+        product={ product }
+        style={ style }
+        ratings={ ratings }
+      />
     </div>
   )
 }
