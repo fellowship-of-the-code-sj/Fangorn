4dc925eb2e8ccdc41f4418df9b9c45826c890941
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-02 10:50:45 -0800

    controler restructuring

---
+++ server/controller/index.js
@@ -0,0 +1,31 @@
+const { retrieveAllRelatedItems } = require('../helperFunctions/helperFunction.js');
+const apiRequest = require('../apiServer/apiRequest.js');
+
+module.exports.relatedItems = (req, res) => {
+  //makes get request to Atelier Products API
+  apiRequest.get(`/products/${req.query.itemId}/related`, (err, data) => {
+    if (err) {
+      res.status(err.response.status);
+      res.end();
+    } else {
+      retrieveAllRelatedItems(data.data, (err, productsData) => {
+        if (err) {
+          //check if error status exists
+          if (err.response) {
+            res.status(err.response.status);
+          } else {
+            res.status(404);
+          }
+          res.end();
+
+        } else {
+          res.status(data.status);
+          res.send(productsData);
+        }
+      })
+    }
+  })
+};
+
+// something like this should be the only things in this file:
+module.exports.getQuestions = require('./getQuestions.js');
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { relatedItems } = require('./controllers.js');
+const { getQuestions, relatedItems } = require('./controller/index.js');
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // Retrieves get request for endpoint /RelatedItems
-app.get('/RelatedItems', relatedItems.get);
+app.get('/RelatedItems', relatedItems);
 
 const PORT = 404;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
