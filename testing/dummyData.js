4b049b8f20ed8f81e055d16da41d7217bb0d34f3
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-02 17:54:43 -0800

    added questions dummy data

--- testing/dummyData.js
+++ testing/dummyData.js
@@ -6,11 +6,11 @@ module.exports = {
       created_at: "2021-02-22T20:24:51.561Z",
       default_price: "69.00",
       description: "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
-      features: (3) [
-        {feature: "Lenses", value: "Ultrasheen"},
-        {feature: "UV Protection", value: null},
-        {feature: "Frames", value: "LightCompose"}
-        ],
+      features: (3)[
+        { feature: "Lenses", value: "Ultrasheen" },
+        { feature: "UV Protection", value: null },
+        { feature: "Frames", value: "LightCompose" }
+      ],
       id: 13024,
       name: "Bright Future Sunglasses",
       slogan: "You've got to wear shades",
@@ -22,11 +22,11 @@ module.exports = {
       created_at: "2021-02-22T20:24:51.561Z",
       default_price: "69.00",
       description: "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
-      features: (3) [
-        {feature: "Lenses", value: "Ultrasheen"},
-        {feature: "UV Protection", value: null},
-        {feature: "Frames", value: "LightCompose"}
-        ],
+      features: (3)[
+        { feature: "Lenses", value: "Ultrasheen" },
+        { feature: "UV Protection", value: null },
+        { feature: "Frames", value: "LightCompose" }
+      ],
       id: 13024,
       name: "Bright Future Sunglasses",
       slogan: "You've got to wear shades",
@@ -41,56 +41,56 @@ module.exports = {
       "sale_price": null,
       "default?": true,
       "photos": [
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
-          }
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+        }
       ],
       "skus": {
-          "370321": {
-              "quantity": 8,
-              "size": "XS"
-          },
-          "370322": {
-              "quantity": 16,
-              "size": "S"
-          },
-          "370323": {
-              "quantity": 17,
-              "size": "M"
-          },
-          "370324": {
-              "quantity": 10,
-              "size": "L"
-          },
-          "370325": {
-              "quantity": 15,
-              "size": "XL"
-          },
-          "370326": {
-              "quantity": 4,
-              "size": "XL"
-          }
+        "370321": {
+          "quantity": 8,
+          "size": "XS"
+        },
+        "370322": {
+          "quantity": 16,
+          "size": "S"
+        },
+        "370323": {
+          "quantity": 17,
+          "size": "M"
+        },
+        "370324": {
+          "quantity": 10,
+          "size": "L"
+        },
+        "370325": {
+          "quantity": 15,
+          "size": "XL"
+        },
+        "370326": {
+          "quantity": 4,
+          "size": "XL"
+        }
       }
     },
     {
@@ -100,86 +100,179 @@ module.exports = {
       "sale_price": "100.00",
       "default?": false,
       "photos": [
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1448526478325-616f2b15b04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1448526478325-616f2b15b04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1519098635131-4c8f806d1e82?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1519098635131-4c8f806d1e82?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1483056293146-9eac9521932f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1483056293146-9eac9521932f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1515992854631-13de43baeba1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1515992854631-13de43baeba1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1525141741567-f89ef016dfeb?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1525141741567-f89ef016dfeb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-          },
-          {
-              "thumbnail_url": "https://images.unsplash.com/photo-1418985991508-e47386d96a71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-              "url": "https://images.unsplash.com/photo-1418985991508-e47386d96a71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-          }
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1448526478325-616f2b15b04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1448526478325-616f2b15b04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1519098635131-4c8f806d1e82?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1519098635131-4c8f806d1e82?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1483056293146-9eac9521932f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1483056293146-9eac9521932f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1515992854631-13de43baeba1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1515992854631-13de43baeba1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1525141741567-f89ef016dfeb?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1525141741567-f89ef016dfeb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+        },
+        {
+          "thumbnail_url": "https://images.unsplash.com/photo-1418985991508-e47386d96a71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "url": "https://images.unsplash.com/photo-1418985991508-e47386d96a71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+        }
       ],
       "skus": {
-          "370345": {
-              "quantity": 8,
-              "size": "XS"
-          },
-          "370346": {
-              "quantity": 16,
-              "size": "S"
-          },
-          "370347": {
-              "quantity": 17,
-              "size": "M"
-          },
-          "370348": {
-              "quantity": 10,
-              "size": "L"
-          },
-          "370349": {
-              "quantity": 15,
-              "size": "XL"
-          },
-          "370350": {
-              "quantity": 6,
-              "size": "XXL"
-          }
+        "370345": {
+          "quantity": 8,
+          "size": "XS"
+        },
+        "370346": {
+          "quantity": 16,
+          "size": "S"
+        },
+        "370347": {
+          "quantity": 17,
+          "size": "M"
+        },
+        "370348": {
+          "quantity": 10,
+          "size": "L"
+        },
+        "370349": {
+          "quantity": 15,
+          "size": "XL"
+        },
+        "370350": {
+          "quantity": 6,
+          "size": "XXL"
+        }
       }
     }
   ],
   reviewMeta: {
     "product_id": "13023",
     "ratings": {
-        "4": "1",
-        "5": "1"
+      "4": "1",
+      "5": "1"
     },
     "recommended": {
-        "false": "1",
-        "true": "1"
+      "false": "1",
+      "true": "1"
     },
     "characteristics": {
-        "Fit": {
-            "id": 43617,
-            "value": "4.0000000000000000"
-        },
-        "Length": {
-            "id": 43618,
-            "value": "3.5000000000000000"
-        },
-        "Comfort": {
-            "id": 43619,
-            "value": "5.0000000000000000"
-        },
-        "Quality": {
-            "id": 43620,
-            "value": "4.0000000000000000"
-        }
+      "Fit": {
+        "id": 43617,
+        "value": "4.0000000000000000"
+      },
+      "Length": {
+        "id": 43618,
+        "value": "3.5000000000000000"
+      },
+      "Comfort": {
+        "id": 43619,
+        "value": "5.0000000000000000"
+      },
+      "Quality": {
+        "id": 43620,
+        "value": "4.0000000000000000"
+      }
     }
+  },
+  questions: {
+    "product_id": "13023",
+    "results": [
+      {
+        "question_id": 66301,
+        "question_body": "Does this product run big or small?",
+        "question_date": "2019-01-17T00:00:00.000Z",
+        "asker_name": "jbilas",
+        "question_helpfulness": 8,
+        "reported": false,
+        "answers": {}
+      },
+      {
+        "question_id": 66303,
+        "question_body": "Can I wash it?",
+        "question_date": "2018-02-08T00:00:00.000Z",
+        "asker_name": "cleopatra",
+        "question_helpfulness": 7,
+        "reported": false,
+        "answers": {
+          "630285": {
+            "id": 630285,
+            "body": "I've thrown it in the wash and it seems fine",
+            "date": "2018-02-08T00:00:00.000Z",
+            "answerer_name": "marcanthony",
+            "helpfulness": 8,
+            "photos": []
+          },
+          "630303": {
+            "id": 630303,
+            "body": "It says not to",
+            "date": "2018-03-08T00:00:00.000Z",
+            "answerer_name": "ceasar",
+            "helpfulness": 0,
+            "photos": []
+          },
+          "630335": {
+            "id": 630335,
+            "body": "I wouldn't machine wash it",
+            "date": "2018-03-08T00:00:00.000Z",
+            "answerer_name": "ceasar",
+            "helpfulness": 0,
+            "photos": []
+          },
+          "630340": {
+            "id": 630340,
+            "body": "Only if you want to ruin it!",
+            "date": "2018-03-08T00:00:00.000Z",
+            "answerer_name": "ceasar",
+            "helpfulness": 5,
+            "photos": []
+          },
+          "630346": {
+            "id": 630346,
+            "body": "Yes",
+            "date": "2018-03-08T00:00:00.000Z",
+            "answerer_name": "Seller",
+            "helpfulness": 4,
+            "photos": []
+          }
+        }
+      },
+      {
+        "question_id": 66302,
+        "question_body": "How long does it last?",
+        "question_date": "2019-07-06T00:00:00.000Z",
+        "asker_name": "funnygirl",
+        "question_helpfulness": 6,
+        "reported": false,
+        "answers": {
+          "630304": {
+            "id": 630304,
+            "body": "It runs small",
+            "date": "2019-11-17T00:00:00.000Z",
+            "answerer_name": "dschulman",
+            "helpfulness": 1,
+            "photos": [
+              "https://images.unsplash.com/photo-1470116892389-0de5d9770b2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+              "https://images.unsplash.com/photo-1536922645426-5d658ab49b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            ]
+          },
+          "630328": {
+            "id": 630328,
+            "body": "Showing no wear after a few months!",
+            "date": "2019-09-06T00:00:00.000Z",
+            "answerer_name": "sillyguy",
+            "helpfulness": 8,
+            "photos": []
+          }
+        }
+      }
+    ]
   }
 }
