904e9eab4eb55fa00b20cbaec909ef8959a609bd
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-02 22:25:13 -0800

    added prop type validation

--- client/src/components/QuestionsAndAnswers/AddQuestionButton.jsx
+++ client/src/components/QuestionsAndAnswers/AddQuestionButton.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const AddQuestions = ({ handleAddQuestionModal }) => {
   return <button onClick={handleAddQuestionModal}>ADD A QUESTION +</button>
 };
 
 export default AddQuestions;
+
+AddQuestions.propTypes = {
+  handleAddQuestionModal: PropTypes.function
+}
