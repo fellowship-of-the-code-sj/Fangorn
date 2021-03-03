b290859e398f41434b9d5015e120c61b90bf26f0
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-03 11:38:24 -0800

    - added state: showMoreAnswersButton, numDisplayedAnswers, displayedAnswers - load up initial rendering of 0, 1, or 2 answers per question - added a handleShowMoreAnswers event handler - will conditionally render the showMoreAnswers button if there are more answers to show - added a click listener to LOAD MORE ANSWERS

--- client/src/components/QuestionsAndAnswers/IndividualQuestion.jsx
+++ client/src/components/QuestionsAndAnswers/IndividualQuestion.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
 import AnswerList from './AnswerList.jsx';
 
 const IndividualQuestion = ({ question }) => {
-  let answers = _.values(question.answers);
+  const [ answers, setAnswers ] = useState([]);
+  const [ showMoreAnswersButton, setShowMoreAnswersButton ] = useState(false);
+  const [ numDisplayedAnswers, setNumDisplayedAnswers ] = useState(0);
+  const [ displayedAnswers, setDisplayedAnswers ] = useState([]);
+
+  useEffect(() => {
+    const allAnswers = _.values(question.answers);
+    setAnswers(allAnswers);
+
+    const numAnswers = allAnswers.length;
+    let numToDisplay;
+    if (numAnswers > 2) {
+      setShowMoreAnswersButton(true);
+      numToDisplay = 2;
+    } else {
+      numToDisplay = numAnswers;
+    }
+    setNumDisplayedAnswers(numToDisplay);
+    setDisplayedAnswers(allAnswers.slice(0, numToDisplay));
+  }, []);
+
+  const handleShowMoreAnswers = e => {
+    e.preventDefault();
+    // there will not be any more answers to display
+    if ((answers.length - numDisplayedAnswers) < 3) {
+      setShowMoreAnswersButton(false);
+      setNumDisplayedAnswers(answers.length);
+      setDisplayedAnswers(answers.slice(0, answers.length));
+    } else {
+    // there will be more answers to display
+      const updatedNum = numDisplayedAnswers + 2;
+      setNumDisplayedAnswers(updatedNum);
+      setDisplayedAnswers(answers.slice(0, updatedNum));
+    }
+  };
 
   return (
     <div>
         <p>Q: {question.question_body}</p>
         <p>Helpful? <a href="#">Yes {question.question_helpfulness}</a></p>
         <a href="#">Add Answer</a>
-        <AnswerList answers={answers} />
+        <AnswerList answers={displayedAnswers} />
+        {
+          showMoreAnswersButton ?
+          <a href="#" onClick={handleShowMoreAnswers}>LOAD MORE ANSWERS</a>
+          : null
+        }
     </div>
   );
 };
