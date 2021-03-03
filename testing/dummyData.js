bb3ad991e30884833e2dd94089258f3dbddf451d
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-02 18:03:17 -0800

    fixed something I missed in dummyData when resolving merge conflict

--- testing/dummyData.js
+++ testing/dummyData.js
@@ -35,19 +35,6 @@ module.exports = {
       campus: "hr-sjo",
       category: "Pants",
       created_at: "2021-02-22T20:24:51.561Z",
-<<<<<<< HEAD
-      default_price: "69.00",
-      description: "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
-      features: (3)[
-        { feature: "Lenses", value: "Ultrasheen" },
-        { feature: "UV Protection", value: null },
-        { feature: "Frames", value: "LightCompose" }
-      ],
-      id: 13024,
-      name: "Bright Future Sunglasses",
-      slogan: "You've got to wear shades",
-      updated_at: "2021-02-22T20:24:51.561Z",
-=======
       default_price: "40.00",
       default_style: {
         'default?': true,
@@ -80,7 +67,6 @@ module.exports = {
       rating: null,
       slogan: "Make yourself a morning person",
       updated_at: "2021-02-22T20:24:51.561Z"
->>>>>>> be838ff3a63b1e215c1435cbe83da7639781779b
     }
   ],
   styles: [
