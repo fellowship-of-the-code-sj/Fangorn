0a4a24ef8c2a474eede49074879553ea9f60769c
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-02 22:17:11 -0800

    updated component AddQuestion to AddQuestionButton

---
+++ client/src/components/QuestionsAndAnswers/AddQuestionButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddQuestions = ({ handleAddQuestionModal }) => {
+  return <button onClick={handleAddQuestionModal}>ADD A QUESTION +</button>
+};
+
+export default AddQuestions;
