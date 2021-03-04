cf8d0f9022c1340ec297a6194b425802e239d670
Adnan Noori <noori.adnan@gmail.com>
2021-03-03 19:34:54 -0800

    - Implemented list functionality for Outfit List Component

--- client/src/components/RelatedOutfit/OutfitList.jsx
+++ client/src/components/RelatedOutfit/OutfitList.jsx
@@ -1,23 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import OutfitListCard from './OutfitListCard.jsx';
+import dummyData from '../../../../testing/dummyData.js';
 
-const OutfitList = ({ outfitList }) => {
+const OutfitList = ({}) => {
+  const [ carouselInput, setCarouselInput ] = useState({start: 0, end: 4});
+
+  if (!window.localStorage.outfitList) {
+    var list = [];
+  } else {
+    var list = JSON.parse(window.localStorage.outfitList);
+  }
+
+  const [ outfitList, setOutfitList ] = useState( { list } )
+
+  //event listener for scrolling right
+  const scrollRight = () => {
+    if (carouselInput.end < outfitList.list.length) {
+      setCarouselInput({start: carouselInput.start + 1, end: carouselInput.end + 1})
+    }
+  }
+
+  //event lisetener for scrolling left
+  const scrollLeft = () => {
+    if (carouselInput.start > 0) {
+      setCarouselInput({start: carouselInput.start - 1, end: carouselInput.end - 1})
+    }
+  }
+
+  //Adds item to outfitList
+  const addOutfit = () => {
+    outfitList.list.push(dummyData.products[0]);
+
+    var outfitListArrayString = JSON.stringify(outfitList.list);
+    window.localStorage.setItem('outfitList', outfitListArrayString);
+
+    setOutfitList({ list: outfitList.list });
+  }
+
+  const removeOutfitItem = (item) => {
+    var index = outfitList.list.indexOf({ id: item})
+    outfitList.list.splice(index, 1);
+
+    var outfitListArrayString = JSON.stringify(outfitList.list);
+    window.localStorage.setItem('outfitList', outfitListArrayString);
+
+    setOutfitList({list: outfitList.list });
+  }
+
   return (
-    <div className='outfitList'>
+    <div className='relatedItemsList'>
+
+      {/* Carousel Scroll Button Left */}
       {
-        outfitList.length ?
-        outfitList.map((item) => {
-          return <OutfitListCard key={item.id} cardData={item.id}></OutfitListCard>
-        })
-        :<div className='outfitListCard'>{'nothing'}</div>
+        carouselInput.start === 0 ?
+        null
+        : <button className='carouselLeftButton' onClick={scrollLeft}> -- </button>
       }
+
+      <div onClick={addOutfit} className='outfitAddItemCard' >{'ADD Outfit'}</div>
+      {
+        outfitList.list.length ?
+        outfitList.list.slice(carouselInput.start, carouselInput.end).map((item) => {
+          return <OutfitListCard cardData={item} removeOutfitItem={removeOutfitItem} ></OutfitListCard>
+        })
+        : null
+      }
+
+      {/* Carousel Scroll Button Right */}
+      {
+        carouselInput.end >= outfitList.list.length ?
+        null
+        : <button className='carouselRightButton' onClick={scrollRight} >--</button>
+      }
+
     </div>
   )
 }
 
 export default OutfitList;
 
-OutfitList.propTypes = {
-  outfitList: PropTypes.array
-}
+// OutfitList.propTypes = {
+//   outfitList: PropTypes.array
+// }
