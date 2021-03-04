45686cb6f75598d47fd90847508026e18124963d
Jaspreet Atwal <jsatwal25@gmail.com>
2021-03-03 21:01:19 -0800

    Refactors map functions to reduce amount of code and improve readability

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
 
 const AddToCart = (props) => {
-  const createQuantities = (obj) => {
-    let quantities = [];
-    for (let key in obj) {
-      quantities.push(obj[key].quantity)
-    }
-    return quantities;
-  }
-  const quantities = createQuantities(props.skus);
+  // const createQuantities = (obj) => {
+  //   let quantities = [];
+  //   for (let key in obj) {
+  //     quantities.push(obj[key].quantity)
+  //   }
+  //   return quantities;
+  // }
+  // const quantities = createQuantities(props.skus);
 
-  const createSizes = (obj) => {
-    let sizes = [];
-    for (let key in obj) {
-      sizes.push(obj[key].size)
-    }
-    return sizes;
-  }
-  const sizes = createSizes(props.skus);
+  // const createSizes = (obj) => {
+  //   let sizes = [];
+  //   for (let key in obj) {
+  //     sizes.push(obj[key].size)
+  //   }
+  //   return sizes;
+  // }
+  // const sizes = createSizes(props.skus);
 
 
   return (
--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -6,8 +6,8 @@ const StyleSelector = (props) => {
     <div className="styleSelector">
       <div id="styleSelectorName">Style: {props.currentStyle.name}</div>
       <div id="styleSelectorStyles">
-        { props.styles.length > 0 ? props.styles.map((style, index) => {
-          return (
+        { props.styles.length > 0 ? 
+          props.styles.map((style, index) => (
             <img
               key={style.style_id}
               className="styleThumbnail"
@@ -21,7 +21,7 @@ const StyleSelector = (props) => {
               index={index}
               id={`styleThumbnail${index}`}
             />
-          )}) : <div></div>
+          )) : null
         }
       </div>
     </div>
