744f2163835b522e6b4ea49986bcb7379ce06ae8
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-04 13:55:07 -0800

    added an onChange event listener

--- client/src/components/QuestionsAndAnswers/Search.jsx
+++ client/src/components/QuestionsAndAnswers/Search.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Search = props => {
+const Search = ({ query, handleQueryInput }) => {
   return (
     <div>
       <input
         type="text"
         name="search"
+        value={query}
+        onChange={handleQueryInput}
         placeholder="Have a question? Search for answers…"></input>
     </div>
   );
 };
 
 export default Search;
+
+Search.propTypes = {
+  query: PropTypes.string,
+  handleQueryInput: PropTypes.func
+};
