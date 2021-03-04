97a2173c927ef07866bb424bd3a54745d82a91b7
Adnan Noori <noori.adnan@gmail.com>
2021-03-04 10:14:52 -0800

    - Created product info request on the top level component

--- client/src/components/ProductDetailPage.jsx
+++ client/src/components/ProductDetailPage.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import serverRequest from '../../serverRequest.js'
+import serverRequest from '../../helperFunctions/serverRequest.js'
 import Overview from './Overview/Overview.jsx'
 import RelatedAndOutfits from './RelatedOutfit/RelatedAndOutfits.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
+import helperFunctions from '../../helperFunctions/helperFunctions.js';
+const PORT = 404;
 
 class ProductDetailPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {productInfo: {}};
   }
 
 
+  componentDidMount() {
+    serverRequest.get(`http://localhost:${PORT}/Overview`, {itemID: 13025}, (result) => {
+      this.setState({ productInfo: result.data })
+    });
+  }
 
   render() {
     return (
@@ -20,7 +28,11 @@ class ProductDetailPage extends React.Component {
         </div>
 
         <div className='secondaryComponent'>
-          <RelatedAndOutfits productID={13025} />
+        {
+          this.state.productInfo.productObj?
+          <RelatedAndOutfits productID={13025} productInfo={helperFunctions.createObjectData(this.state.productInfo)} />
+          : <RelatedAndOutfits productID={13025} />
+        }
           <QuestionsAndAnswers productID={13024} />
           <RatingsAndReviews productID={13023} />
         </div>
