b9b97985b09177946b41f508e526d2b6bed87c05
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-04 10:55:03 -0800

    Makes API POST request to add question

--- server/models/questions.js
+++ server/models/questions.js
@@ -3,6 +3,7 @@ const { url } = require('../apiURL.js');
 
 module.exports = {
   getAllQuestions: (product_id) => axios.get(`${url}/qa/questions/?product_id=${product_id}`),
+  postQuestion: (body_params) => axios.post(`${url}/qa/questions`, body_params),
   putQuestionHelpful: (question_id) => axios.put(`${url}/qa/questions/${question_id}/helpful`),
   putQuestionReport: (question_id) => axios.put(`${url}/qa/questions/${question_id}/report`),
   putAnswerHelpful: (answer_id) => axios.put(`${url}/qa/answers/${answer_id}/helpful`),
