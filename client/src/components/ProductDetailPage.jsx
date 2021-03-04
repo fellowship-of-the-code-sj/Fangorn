ef5f463c80d74a298296511022384ccfc704d51d
Evan <evanjordan42@gmail.com>
2021-03-03 16:00:28 -0800

    Added view more reviews button and functionality

--- client/src/components/ProductDetailPage.jsx
+++ client/src/components/ProductDetailPage.jsx
@@ -16,7 +16,7 @@ class ProductDetailPage extends React.Component {
         <Overview productID={13023} />
         <RelatedAndOutfits productID={13025} />
         <QuestionsAndAnswers productID={13024} />
-        <RatingsAndReviews productID={13026} />
+        <RatingsAndReviews productID={13023} />
       </div>
     )
   }
