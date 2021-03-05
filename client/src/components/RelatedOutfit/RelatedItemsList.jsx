560442d9b3bef86f5e234b6263708b5a3dac62ce
Adnan Noori <noori.adnan@gmail.com>
2021-03-04 17:31:11 -0800

    - added carousel thingy to outfit list

--- client/src/components/RelatedOutfit/RelatedItemsList.jsx
+++ client/src/components/RelatedOutfit/RelatedItemsList.jsx
@@ -3,62 +3,47 @@ import PropTypes from 'prop-types';
 import RelatedItemCard from './RelatedItemCard.jsx';
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
+import helperFunctions from '../../../helperFunctions/helperFunctions.js';
+import ComparisonTable from './ComparisonTable.jsx';
 
 const RelatedItemsList = ({ relatedItemsList, productInfo }) => {
 
-  //state for controlling the carousel
-  const [ carouselInput, setCarouselInput] = useState({start: 0, end: 4});
+  const [ actionButtonToggle, setActionButtonToggle ] = useState(false);
 
-  //event listener for scrolling right
-  const scrollRight = () => {
-    if (carouselInput.end < relatedItemsList.length) {
-      setCarouselInput({start: carouselInput.start + 1, end: carouselInput.end + 1})
-    }
+  const [comparisonData, setComparisonData] = useState({});
+
+   //event listener for action button
+  const actionButtonListener = (comparedItem) => {
+    setComparisonData(comparedItem);
+    setActionButtonToggle(!actionButtonToggle);
   }
-
-  //event lisetener for scrolling left
-  const scrollLeft = () => {
-    if (carouselInput.start > 0) {
-      setCarouselInput({start: carouselInput.start - 1, end: carouselInput.end - 1})
-    }
-  }
-
 
   return (
     <div className='relatedItemsList'>
       {
         relatedItemsList.length?
-        <Carousel responsive={responsive}>
+        <Carousel responsive={helperFunctions.responsive}>
           {
             relatedItemsList.map((item) => {
-              return <RelatedItemCard productInfo={productInfo} key={item.id} cardData={item}></RelatedItemCard>;
+              return <RelatedItemCard actionButtonListener={actionButtonListener} productInfo={productInfo} key={item.id} cardData={item}></RelatedItemCard>;
             })
           }
 
         </Carousel>
         : null
       }
-
-      {/* Carousel Scroll Button Left */}
-      {/* {
-        carouselInput.start === 0 ?
-        null
-        : <button className='carouselLeftButton' onClick={scrollLeft}> -- </button>
-      } */}
-      {/* {
-        relatedItemsList.length ?
-        relatedItemsList.slice(carouselInput.start, carouselInput.end).map((item) => {
-          return <RelatedItemCard productInfo={productInfo} key={item.id} cardData={item}></RelatedItemCard>;
-        })
-        : null
-      } */}
-
-      {/* Carousel Scroll Button Right */}
-      {/* {
-        carouselInput.end >= relatedItemsList.length ?
-        null
-        : <button className='carouselRightButton' onClick={scrollRight} >--</button>
-      } */}
+      {
+        actionButtonToggle ?
+          <div>
+            <div className='outerModal' onClick={() => {actionButtonListener()}}></div>
+            <ComparisonTable
+              comparisionList={ helperFunctions.comparisonTable(productInfo.features, comparisonData.features)}
+              currentProductName={productInfo.name}
+              relatedProductName={comparisonData.name}
+            />
+          </ div>
+          : null
+      }
     </div>
   )
 };
@@ -69,24 +54,3 @@ RelatedItemsList.propTypes = {
   relatedItemsList: PropTypes.array,
   productInfo: PropTypes.object
 }
-
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 4
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 4
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 4
-  }
-};
