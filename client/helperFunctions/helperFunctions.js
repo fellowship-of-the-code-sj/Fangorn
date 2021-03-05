24bb3b21ed5197add484bef9ed7b86e881783bf1
Adnan Noori <noori.adnan@gmail.com>
2021-03-04 17:29:50 -0800

    - Updated to make code cleaner

--- client/helperFunctions/helperFunctions.js
+++ client/helperFunctions/helperFunctions.js
@@ -53,4 +53,24 @@ const comparisonTable = (currentProduct, relatedProduct) => {
   return resultObj;
  }
 
-export default { createProductObjectData, comparisonTable };
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 4
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 4
+  }
+};
+
+export default { createProductObjectData, comparisonTable, responsive };
