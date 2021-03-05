170cf0a5bdce73d4dbbe93fffd1b9dcf12add2bd
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-05 14:43:34 -0800

    added underscore dependency

--- client/src/components/QuestionsAndAnswers/IndividualAnswer.jsx
+++ client/src/components/QuestionsAndAnswers/IndividualAnswer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import _ from 'underscore';
 
 const IndividualAnswer = ({ answer }) => {
   // this should really be handled by the API..
