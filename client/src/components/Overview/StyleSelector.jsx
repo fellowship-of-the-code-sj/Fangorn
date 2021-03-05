b44b9a33a7325b13c2f73a6a1d7567119fd88adf
Jaspreet Atwal <jsatwal25@gmail.com>
2021-03-04 16:19:59 -0800

    Sets up grid for Product Info and Style Selector

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -4,24 +4,25 @@ import PropTypes from 'prop-types';
 const StyleSelector = (props) => {
   return (
     <div className="styleSelector">
-      <div id="styleSelectorName">Style: {props.currentStyle.name}</div>
+      <div id="styleSelectorName">Style &gt; {props.currentStyle.name}</div>
       <div id="styleSelectorStyles">
         { props.styles.length > 0 ? 
           props.styles.map((style, index) => (
-            <img
-              key={style.style_id}
-              className="styleThumbnail"
-              onClick={event => {
-                // console.log(event.target.attributes)
-                props.handleStyleChange(event.target.attributes[4].value)
-              }}
-              src={style.photos[0].thumbnail_url}
-              alt={`Image Thumbnail ${style.name}`}
-              height="50"
-              // width="50"
-              index={index}
-              id={`styleThumbnail${index}`}
-            />
+            <div key={style.style_id} className="styleContainer">
+              <img
+                className="styleThumbnail"
+                onClick={event => {
+                  // console.log(event.target.attributes)
+                  props.handleStyleChange(event.target.attributes[3].value)
+                }}
+                src={style.photos[0].thumbnail_url}
+                alt={`Image Thumbnail ${style.name}`}
+                // height="50"
+                // width="50"
+                index={index}
+                id={`styleThumbnail${index}`}
+              />
+            </div>
           )) : null
         }
       </div>
