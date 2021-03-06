0acf81dd7644bdea7ec4aa97524cd0f420c27a4a
Adnan Noori <noori.adnan@gmail.com>
2021-03-06 10:04:26 -0800

    - Updated dependencies for react-slick and modified the configurations

--- testing/setupTests.js
+++ testing/setupTests.js
@@ -2,4 +2,12 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'babel-polyfill';
 
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches : false,
+    addListener : function() {},
+    removeListener: function() {}
+  };
+};
+
 configure({ adapter: new Adapter() });
