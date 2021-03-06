4e464a01db70166d0a8a44ccf90262ceef864608
Adnan Noori <noori.adnan@gmail.com>
2021-03-06 10:06:53 -0800

    - Updated Comparison table stucture and styling

--- client/src/components/RelatedOutfit/ComparisonTable.jsx
+++ client/src/components/RelatedOutfit/ComparisonTable.jsx
@@ -3,37 +3,39 @@ import PropTypes from 'prop-types';
 
 const ComparisonTable = ({ comparisionList, currentProductName, relatedProductName }) => {
   return (
-    <table className='innerModal'>
-      <tr className='comparingTitle' >COMPARING</tr>
-      <thead className='modalHead'>
-        <tr>
-          <th className='leftTitle' >{currentProductName}</th>
-          <th></th>
-          <th className='rightTitle' >{relatedProductName}</th>
-        </tr>
-      </thead>
-      <tbody className='modalBody'>
-        {
-          Object.keys(comparisionList).map((feature, index) => {
-            return (
-            <tr key={index}>
-              {
-                comparisionList[feature][0]?
-                <td className='leftCheckMark' ><ion-icon name="checkmark-outline"></ion-icon></td>
-                : <td className='leftCheckMark' > </td>
-              }
-              <td className='centerFeature' >{feature}</td>
-              {
-                comparisionList[feature][1]?
-                <td className='rightCheckMark' ><ion-icon name="checkmark-outline"></ion-icon></td>
-                : <td className='rightCheckMark' > </td>
-              }
-            </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <div className='innerModal'>
+      <div className='comparingItems'>COMPARING</div>
+      <table >
+        <thead className='modalHead'>
+          <tr>
+            <th className='leftTitle' >{currentProductName}</th>
+            <th></th>
+            <th className='rightTitle' >{relatedProductName}</th>
+          </tr>
+        </thead>
+        <tbody className='modalBody'>
+          {
+            Object.keys(comparisionList).map((feature, index) => {
+              return (
+              <tr key={index}>
+                {
+                  comparisionList[feature][0]?
+                  <td className='leftCheckMark' ><ion-icon name="checkmark-outline"></ion-icon></td>
+                  : <td className='leftCheckMark' > </td>
+                }
+                <td className='centerFeature' >{feature}</td>
+                {
+                  comparisionList[feature][1]?
+                  <td className='rightCheckMark' ><ion-icon name="checkmark-outline"></ion-icon></td>
+                  : <td className='rightCheckMark' > </td>
+                }
+              </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
