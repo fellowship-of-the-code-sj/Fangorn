3e8f03ce87494aa79ae8d9ed8b943c4066445ec1
Jaspreet Atwal <jsatwal25@gmail.com>
2021-03-05 20:40:13 -0800

    Refactors quantity select to work properly

--- client/src/components/Overview/NewATC.jsx
+++ client/src/components/Overview/NewATC.jsx
@@ -25,22 +25,67 @@ const ATC = ({ skus }) => {
     setCurrentQty(null);
   }, [skus])
 
+  const sizeChange = (sizeString) => {
+    if (sizeString === 'Select Size') {
+      setCurrentSku({
+        size: sizeString,
+        quantities: []
+      })
+    }
+    for (let key in skus) {
+      if (skus[key].size === sizeString) {
+        setCurrentSku({
+          size: sizeString,
+          quantities: createArray(skus[key].quantity)
+        })
+        if (!currentQty) {
+          setCurrentQty('1')
+        }
+      }
+    }
+  }
+
+  const quantityChange = (qtyString) => {
+    setCurrentQty(qtyString);
+  }
+
+  const createArray = (qty) => {
+    let quantities = [];
+    if (qty < 15) {
+      for (let i = 1; i <= qty; i++) {
+        quantities.push(i);
+      }
+    } else {
+      for (let i = 1; i <= 15; i++) {
+        quantities.push(i);
+      }
+    }
+    return quantities;
+  }
+
   return (
     <div className="addToCart">
       {sizes.length > 0 ?
-      <select 
-        id="sizeSelect"
-        onChange={(event) => (console.log(sizes))}>
-        <option>Select Size</option>
-        {sizes.map((size, i) => (
-          <option key={i}>{size}</option>
-        ))}
-      </select>
-      :
-      <select id="sizeSelect" disabled><option>OUT OF STOCK</option></select>}
-      <select id="quantitySelect">
-        <option>-</option>
-      </select>
+        <select 
+          id="sizeSelect"
+          onChange={(e) => {sizeChange(e.target.value)}}>
+          <option>Select Size</option>
+          {sizes.map((size, i) => (
+            <option key={i}>{size}</option>
+          ))}
+        </select> :
+        <select id="sizeSelect" disabled><option>OUT OF STOCK</option></select>
+      }
+      {currentSku.quantities.length > 0 ? 
+        <select
+          id="quantitySelect"
+          onChange={(e) => {setCurrentQty(e.target.value)}}>
+          {currentSku.quantities.map((quantity, i) => (
+            <option key={i}>{quantity}</option>
+          ))}
+        </select> :
+        <select id="quantitySelect"><option>-</option></select>
+      }
       <button id="addToCartButton">ADD TO CART +</button>
     </div>
   )
