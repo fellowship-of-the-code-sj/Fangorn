b39349c5a78aea61085fa18d0ba64838082a96db
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-09 16:52:35 -0800

    captured interactions now being sent to server

--- client/src/hoc/captureInteractions.js
+++ client/src/hoc/captureInteractions.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import axios from 'axios';
 
-const captureInteractions = widget => {
-  return function decorator(WrappedComponent) {
-    return function decoratedComponent(props) {
-      const logger = e => {
-        const currentDate = new Date();
-        console.log('stealing your data..');
-        console.log('\telement:', e.target.nodeName);
-        console.log('\ttime:', currentDate.toUTCString());
-        console.log('\twidget:', widget);
-        console.log('thanks bro');
-      };
-
-      return <WrappedComponent logger={logger} {...props} />
-    }
-  };
-};
-
 // const captureInteractions = widget => {
 //   return function decorator(WrappedComponent) {
 //     return function decoratedComponent(props) {
 //       const logger = e => {
 //         const currentDate = new Date();
 //         console.log('stealing your data..');
-//         const bodyParams = {
-//           element: e.target.nodeName,
-//           time: currentDate.toUTCString(),
-//           widget
-//         };
-
-//         axios.post('http://localhost:404/interactions/', bodyParams)
-//           .then(() => console.log('logged'))
-//           .catch(err => console.error(err));
+//         console.log('\telement:', e.target.nodeName);
+//         console.log('\ttime:', currentDate.toUTCString());
+//         console.log('\twidget:', widget);
+//         console.log('thanks bro');
 //       };
 
 //       return <WrappedComponent logger={logger} {...props} />
 //     }
 //   };
 // };
+
+const captureInteractions = widget => {
+  return function decorator(WrappedComponent) {
+    return function decoratedComponent(props) {
+      const logger = e => {
+        const currentDate = new Date();
+        console.log('stealing your data..');
+        const bodyParams = {
+          element: e.target.nodeName,
+          time: currentDate.toUTCString(),
+          widget
+        };
+
+        axios.post('http://localhost:404/interactions/', bodyParams)
+          .then(() => console.log('logged'))
+          .catch(err => console.error(err));
+      };
+
+      return <WrappedComponent logger={logger} {...props} />
+    }
+  };
+};
 
 export default captureInteractions;
