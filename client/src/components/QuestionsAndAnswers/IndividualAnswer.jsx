60bb25db514b3a67447e0198982a894e3ad6897a
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-13 10:40:24 -0800

    added classes to prevent answer text from showing up beyond the viewport

--- client/src/components/QuestionsAndAnswers/IndividualAnswer.jsx
+++ client/src/components/QuestionsAndAnswers/IndividualAnswer.jsx
@@ -42,10 +42,7 @@ const IndividualAnswer = ({ answer, logger }) => {
 
   return (
     <div className="answer">
-      <div className="flex">
-        <div className="answer-body">{_.unescape(answer.body)}</div>
-        <div className="flex-grow"></div>
-      </div>
+      <div className="answer-body">{_.unescape(answer.body)}</div>
       <div className="secondary-text flex">
         <div>
             by <span style={bolded}>{_.unescape(answer.answerer_name)}</span>, {date}
