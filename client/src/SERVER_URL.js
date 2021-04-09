f7881ab7261660dd9d44abcafe6a59f7417e48c0
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-08 19:15:20 -0700

    added server url as a process.env variable

--- client/src/SERVER_URL.js
+++ client/src/SERVER_URL.js
@@ -1,3 +1,4 @@
-const SERVER_URL = 'http://ec2-18-188-110-174.us-east-2.compute.amazonaws.com';
+// const SERVER_URL = 'http://ec2-18-188-110-174.us-east-2.compute.amazonaws.com';
+const SERVER_URL = process.env.URL;
 
 export default SERVER_URL;
